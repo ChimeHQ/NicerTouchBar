5e308519776544e22cca6a267abf0eafe2bb204b
Matt Massicotte <85322+mattmassicotte@users.noreply.github.com>
2019-06-01 19:57:09 -0400

    Added some comment docs

--- NicerTouchBar/NSViewController+Binding.swift
+++ NicerTouchBar/NSViewController+Binding.swift
@@ -9,12 +9,25 @@
 import Cocoa
 
 public extension NSViewController {
+
+    /// Clears bindings for the "touchBar" keypath on the host window.
+    ///
+    /// This function removes any installed bingings on the "touchBar" keypath for
+    /// the window hosting this controllers' view.
+    ///
+    /// It does nothing if invoked on < 10.12.2
     func restoreParentWindowTouchBar() {
         if #available(OSX 10.12.2, *) {
             view.window?.unbind(NSBindingName(rawValue: #keyPath(touchBar)))
         }
     }
 
+    /// Binds this controller's touchBar to the host window
+    ///
+    /// This function installs a binding in the window hosting this controllers' view
+    /// to this controller's touchBar property.
+    ///
+    /// It does nothing if invoked on < 10.12.2
     func becomeParentWindowTouchBarProvider() {
         restoreParentWindowTouchBar()
 
